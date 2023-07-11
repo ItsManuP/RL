d58829b61ad3a5a3e1f695f7a078fa9dd6318414
ItsManuP <46021610+ItsManuP@users.noreply.github.com>
2023-07-11 10:37:07 +0200

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ module.exports = {
   theme: {
     
     extend: {
-      
+      animation: {
+        'pulse': 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite '
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
